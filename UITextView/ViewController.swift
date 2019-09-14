c2d0c96f9c0b3c7ae4ba8efb12acea95fa8f08db
Burenkovuser <nb2207@yandex.ru>
2019-09-14 11:52:44 +0700

    Initial Commit

---
+++ UITextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextView
+//
+//  Created by Vasilii on 14/09/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
